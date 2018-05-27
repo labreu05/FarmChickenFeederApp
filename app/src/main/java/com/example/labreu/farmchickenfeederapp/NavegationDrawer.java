336ea04ccfb24cad750968bb8c92be2d802635c3
Leonardo Abreu <leonardoeap@gmail.com>
2018-05-27 17:11:09 -0400

    Remove settings from drawer

--- app/src/main/java/com/example/labreu/farmchickenfeederapp/NavegationDrawer.java
+++ app/src/main/java/com/example/labreu/farmchickenfeederapp/NavegationDrawer.java
@@ -53,13 +53,6 @@ public class NavegationDrawer extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navegation_drawer, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
