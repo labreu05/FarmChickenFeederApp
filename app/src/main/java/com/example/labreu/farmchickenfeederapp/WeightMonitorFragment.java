e3ec7ee444c2907d59588a5a30e59001a533137b
Leonardo Abreu <leonardoeap@gmail.com>
2018-05-29 22:49:25 -0400

    Connect main container to firebase

--- app/src/main/java/com/example/labreu/farmchickenfeederapp/WeightMonitorFragment.java
+++ app/src/main/java/com/example/labreu/farmchickenfeederapp/WeightMonitorFragment.java
@@ -1,16 +1,45 @@
 package com.example.labreu.farmchickenfeederapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class WeightMonitorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weight_monitor, container, false);
+        // Set Main Container connection to Firebase
+        final View view = inflater.inflate(R.layout.fragment_weight_monitor, container, false);
+        final TextView pounds = (TextView) view.findViewById(R.id.pounds);
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mainRef = dbRef.child("mainContainer").child("pounds");
+        mainRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+
+                pounds.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
     }
 }
