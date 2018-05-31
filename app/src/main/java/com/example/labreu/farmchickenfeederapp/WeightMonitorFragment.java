7279439aef74b80d88f993917c4925a615d064d1
Leonardo Abreu <leonardoeap@gmail.com>
2018-05-31 12:06:15 -0400

    Connect Weight Monitor Activity to Firebase

--- app/src/main/java/com/example/labreu/farmchickenfeederapp/WeightMonitorFragment.java
+++ app/src/main/java/com/example/labreu/farmchickenfeederapp/WeightMonitorFragment.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,17 +23,41 @@ public class WeightMonitorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Set Main Container connection to Firebase
-        final View view = inflater.inflate(R.layout.fragment_weight_monitor, container, false);
-        final TextView pounds = (TextView) view.findViewById(R.id.pounds);
+        // Get Firebase References
+        // Main Container References
         DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference mainRef = dbRef.child("mainContainer").child("pounds");
-        mainRef.addValueEventListener(new ValueEventListener() {
+        DatabaseReference mainContainerPoundsRef = dbRef.child("mainContainer").child("pounds");
+        // Plate 1 References
+        DatabaseReference plate1Ref = dbRef.child("plates").child("plate1");
+        DatabaseReference plate1PoundsRef = plate1Ref.child("pounds");
+        final DatabaseReference plate1StatusRef = plate1Ref.child("isActive");
+        DatabaseReference plate1LastFillRef = plate1Ref.child("lastFill");
+        // Plate 2 References
+        DatabaseReference plate2Ref = dbRef.child("plates").child("plate2");
+        DatabaseReference plate2PoundsRef = plate2Ref.child("pounds");
+        final DatabaseReference plate2StatusRef = plate2Ref.child("isActive");
+        DatabaseReference plate2LastFillRef = plate2Ref.child("lastFill");
+
+        // Get views
+        final View mainView = inflater.inflate(R.layout.fragment_weight_monitor, container, false);
+        final TextView mainContainerPounds = (TextView) mainView.findViewById(R.id.pounds);
+        // Plate 1
+        final TextView plate1Pounds = (TextView) mainView.findViewById(R.id.plate_1_pounds);
+        final TextView plate1LastFill = (TextView) mainView.findViewById(R.id.plate_1_last_fill);
+        final Switch plate1Status = (Switch) mainView.findViewById(R.id.plate_1_status);
+        // Plate 2
+        final TextView plate2Pounds = (TextView) mainView.findViewById(R.id.plate_2_pounds);
+        final TextView plate2LastFill = (TextView) mainView.findViewById(R.id.plate_2_last_fill);
+        final Switch plate2Status = (Switch) mainView.findViewById(R.id.plate_2_status);
+
+        // Set Value event listeners
+        // Main Container
+        mainContainerPoundsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String text = dataSnapshot.getValue(String.class);
 
-                pounds.setText(text);
+                mainContainerPounds.setText(text);
             }
 
             @Override
@@ -39,7 +65,102 @@ public class WeightMonitorFragment extends Fragment {
 
             }
         });
+        // Plate 1
+        plate1PoundsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
 
-        return view;
+                plate1Pounds.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        plate1StatusRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Boolean status = dataSnapshot.getValue(Boolean.class);
+
+                plate1Status.setChecked(status);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        plate1LastFillRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+
+                plate1LastFill.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        // Plate 2
+        plate2PoundsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+
+                plate2Pounds.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        plate2StatusRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Boolean status = dataSnapshot.getValue(Boolean.class);
+
+                plate2Status.setChecked(status);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        plate2LastFillRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+
+                plate2LastFill.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        // Set Value Change Listener
+        // Plate 1
+        plate1Status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                plate1StatusRef.setValue(isChecked);
+            }
+        });
+        // Plate 2
+        plate2Status.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                plate2StatusRef.setValue(isChecked);
+            }
+        });
+
+        return mainView;
     }
 }
