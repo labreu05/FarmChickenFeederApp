42aae1391139277de493acb7dc757e6e44ba90cd
Leonardo Abreu Peña <leonardo.abreu@intellisys.com.do>
2018-07-12 14:08:04 -0400

    Add date picker dialogs to reports page

--- app/src/main/java/com/example/labreu/farmchickenfeederapp/ReportsFragment.java
+++ app/src/main/java/com/example/labreu/farmchickenfeederapp/ReportsFragment.java
@@ -1,17 +1,60 @@
 package com.example.labreu.farmchickenfeederapp;
 
 
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class ReportsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View mainView = inflater.inflate(R.layout.fragment_reports, container, false);
+
+        final EditText startDate = mainView.findViewById(R.id.start_date);
+        final EditText endDate = mainView.findViewById(R.id.end_date);
+        Date date = new Date();
+        final Calendar calendar = Calendar.getInstance();
+
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePicker = new DatePickerDialog(mainView.getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        startDate.setText(day+"-"+(month+1)+"-"+year);
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)-1, calendar.get(Calendar.DAY_OF_MONTH));
+
+                datePicker.show();
+            }
+        });
+
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePicker = new DatePickerDialog(mainView.getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        endDate.setText(day+"-"+(month+1)+"-"+year);
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)-1, calendar.get(Calendar.DAY_OF_MONTH));
+
+                datePicker.show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reports, container, false);
+        return mainView;
     }
 }
